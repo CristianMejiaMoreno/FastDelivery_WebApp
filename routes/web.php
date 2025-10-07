53c1ae77684df927eb377c830ce0e3894971c375
CristianMejiaMoreno <cristianmejia88w@gmail.com>
2025-10-07 09:15:56 -0500

    feature / routes / creacion de routes API para Repartidor y actualizacion de web routes con resources para Clientes, Pedidos, and Ventas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\PedidoController;
 use App\Http\Controllers\RepartidoresController;
+use App\Http\Controllers\VentasController;
+use App\Http\Controllers\ZonaController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -13,7 +17,21 @@ Route::middleware(['auth', 'verified'])->group(function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
 
+    Route::get('repartidores/list', [RepartidoresController::class, 'list'])->name('repartidores.list');
     Route::resource('repartidores', RepartidoresController::class);
+
+    Route::get('zonas/list', [ZonaController::class, 'list'])->name('zona.list');
+    Route::resource('zonas', ZonaController::class);
+
+    Route::get('clientes/list', [ClienteController::class, 'list'])->name('clientes.list');
+    Route::resource('clientes', ClienteController::class);
+
+    Route::get('pedidos/list', [PedidoController::class, 'list'])->name('pedidos.list');
+    Route::resource('pedidos', PedidoController::class);
+
+    Route::get('ventas/list', [VentasController::class, 'list'])->name('ventas.list');
+    Route::resource('ventas', VentasController::class);
+
 });
 
 require __DIR__.'/settings.php';
